0ba3dce945515d4a1bf5f84187783276253e9052
RafaelaFernandaSilva <rafaelafernandasilveira@gmail.com>
2021-05-13 03:18:48 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/rafaelafernanda/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.rafaelafernanda.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rafaelafernanda.cursomc.domain.Categoria;
+
+@Repository
+public interface  CategoriaRepository extends JpaRepository<Categoria, Integer> {
+	
+	
+
+}
